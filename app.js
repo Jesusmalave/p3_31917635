243cc1ab671bb0c872b9f61abe08c841b0f08375
Jesusmalave <jesusmalave780@gmail.com>
2024-06-08 12:04:55 -0400

    Proyecto

--- app.js
+++ app.js
@@ -21,6 +21,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/product', (req, res) => {
+  // Lógica para renderizar la vista "gallery.jade"
+  res.render('product');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
